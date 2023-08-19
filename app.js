9e4667a11be6356806a19dc669f8084bef0145cf
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-08-19 20:23:56 +0600

    add update

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = new express();
 const bodyParser = require("body-parser");
 
 //! Security Middleware
-
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
@@ -13,12 +12,10 @@ const hpp = require("hpp");
 const cors = require("cors");
 
 //! ENV
-
 const dotENV = require("dotenv");
 dotENV.config();
 
 //! Security Middleware implements
-
 app.use(helmet());
 app.use(mongoSanitize());
 app.use(xss());
@@ -28,11 +25,9 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "100mb" }));
 
 //! BodyParser implements
-
 app.use(bodyParser.json());
 
 //! Rate Limit
-
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 3000,
@@ -40,11 +35,9 @@ const limiter = rateLimit({
 app.use(limiter);
 
 //! Database
-
 const mongoose = require("mongoose");
 
 //! Database Connect
-
 const URL = `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_USER_PASSWORD}@cluster0.fsp0qs4.mongodb.net/Cart-Project?retryWrites=true&w=majority`;
 
 
@@ -59,5 +52,4 @@ mongoose.connect(
 
 //! Frontend URL Tagging API
 app.use("/api", router);
-
 module.exports = app;
